d811e6569b792bf1a17c48e3cb13394d888372aa
Reid Miller <reid.s.miller@gmail.com>
2023-01-04 14:53:28 -0700

    Add 4 methods

--- arrays.rb
+++ arrays.rb
@@ -3,3 +3,19 @@ ages = [10, 25, 32, 46, 91]
 ave_costs = [14.1, 15.2, 12.9, 16.8, 10.3]
 committed = [true, true, false, true]
 
+# pop will remove and return last element "Elizabeth"
+friends.pop
+puts friends
+
+# push will add the element 102 to the end of the ages array
+ages.push(102)
+puts ages
+
+# shift will remove and return first element 10.3
+ave_costs.shift
+puts ave_costs
+
+# unshift will add element false to the beginning of the array
+committed.unshift(false)
+puts committed
+
