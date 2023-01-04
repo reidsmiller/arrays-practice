ee0566406e63cf46267d5f65e69381d0dd30e3a9
Reid Miller <reid.s.miller@gmail.com>
2023-01-04 14:58:26 -0700

    Actually add index position description

--- arrays.rb
+++ arrays.rb
@@ -5,17 +5,17 @@ committed = [true, true, false, true]
 
 # pop will remove and return last element "Elizabeth"
 friends.pop
-friends
 
 # push will add the element 102 to the end of the ages array
 ages.push(102)
-ages
 
 # shift will remove and return first element 10.3
 ave_costs.shift
-ave_costs
 
 # unshift will add element false to the beginning of the array
 committed.unshift(false)
-committed
+
+# Index positions start at 0 with the first element in the array, 1 with the second element, and so forth.
+# If I were to call Index Position 2 on friends, it will return "Matt"
+puts friends[2]
 
