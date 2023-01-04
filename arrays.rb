13f59d7d86c19a09ec1642f74307b1e17524c606
Reid Miller <reid.s.miller@gmail.com>
2023-01-04 14:57:41 -0700

    Add index position description

--- arrays.rb
+++ arrays.rb
@@ -5,17 +5,17 @@ committed = [true, true, false, true]
 
 # pop will remove and return last element "Elizabeth"
 friends.pop
-puts friends
+friends
 
 # push will add the element 102 to the end of the ages array
 ages.push(102)
-puts ages
+ages
 
 # shift will remove and return first element 10.3
 ave_costs.shift
-puts ave_costs
+ave_costs
 
 # unshift will add element false to the beginning of the array
 committed.unshift(false)
-puts committed
+committed
 
